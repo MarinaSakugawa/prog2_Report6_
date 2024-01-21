adfe073776e98ea52c956d258a1f2b4a805d2739
Marina Sakugawa <e235724@ie.u-ryukyu.ac.jp>
2024-01-21 18:09:04 +0900

    step2

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235724/Deck.java
@@ -0,0 +1,38 @@
+package jp.ac.uryukyu.ie.e235724;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    private ArrayList<Card> cards = new ArrayList<>();
+
+    ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    Deck() {
+
+    }
+
+    public void initializeDeck() {
+        String[] suits = {"♠︎", "♣︎", "♥︎", "♦︎"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+        for(String suit : suits) {
+            for(String rank : ranks) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        if(cards.isEmpty()) {
+            throw new IllegalStateException("Deck is Empty");
+        }
+        return cards.remove(0);
+    }
+}
