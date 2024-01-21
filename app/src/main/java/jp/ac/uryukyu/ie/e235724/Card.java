de607bc1ae7f89f3c09220501c381bb51a468f39
Marina Sakugawa <e235724@ie.u-ryukyu.ac.jp>
2024-01-21 17:21:00 +0900

    step1

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235724/Card.java
@@ -0,0 +1,23 @@
+package jp.ac.uryukyu.ie.e235724;
+
+public class Card {
+    private String suit;
+    private String rank;
+
+    Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    String getSuit() {
+        return suit;
+    }
+
+    String getRank() {
+        return rank;
+    }
+
+    public String toString() {
+        return rank + "of" + suit;
+    }
+}
