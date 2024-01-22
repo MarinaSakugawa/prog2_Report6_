ebf96da54f93afb621b4c8f718baee7a634c5067
Marina Sakugawa <e235724@ie.u-ryukyu.ac.jp>
2024-01-22 23:21:50 +0900

    step14

--- app/src/main/java/jp/ac/uryukyu/ie/e235724/BlackJackGame.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235724/BlackJackGame.java
@@ -1,7 +1,25 @@
 package jp.ac.uryukyu.ie.e235724;
 
+/*
+ * ブラックジャックゲームの実装．
+ * 
+ * ゲームの勝者判定をする．
+ */
+
 public class BlackJackGame {
 
+    /*
+     * 勝利判定を行う．
+     * プレイヤーとディーラーの手札のスコアを比較して、ゲームの結果を返す．
+     *
+     * @param playerHand プレイヤーの手札
+     * @param dealerHand ディーラーの手札
+     * @return 勝者判定に基づく結果の文字列
+     *         - "You win!!": プレイヤーの勝利
+     *         - "This game is draw...": 引き分け
+     *         - "Dealer wins!!": ディーラーの勝利
+     *         - "You burst! Dealer wins!!": プレイヤーがバーストした場合のディーラーの勝利
+     */
     public String determineWinner(Hand playerHand, Hand dealerHand) {
         int playerScore = playerHand.calculateScore();
         int dealerScore = dealerHand.calculateScore();
