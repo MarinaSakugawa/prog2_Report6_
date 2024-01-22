cb12b257494027c53189fbb4b5490ebc17a52903
Marina Sakugawa <e235724@ie.u-ryukyu.ac.jp>
2024-01-22 23:52:52 +0900

    step17

--- app/src/main/java/jp/ac/uryukyu/ie/e235724/Card.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235724/Card.java
@@ -1,22 +1,55 @@
 package jp.ac.uryukyu.ie.e235724;
 
+/**
+ * カードを表すクラス．
+ */
 public class Card {
+
+    /**
+     * カードのスート（マーク）を表す文字列．
+     */
     private String suit;
+
+    /**
+     * カードのランク（数字または絵柄）を表す文字列．
+     */
     private String rank;
 
+    /**
+     * Card クラスのコンストラクタ．
+     * 
+     * @param suit カードのスート
+     * @param rank カードのランク
+     */
     Card(String suit, String rank) {
         this.suit = suit;
         this.rank = rank;
     }
 
+    /**
+     * カードのスートを取得．
+     * 
+     * @return カードのスート
+     */
     String getSuit() {
         return suit;
     }
 
+    /**
+     * カードのランクを取得．
+     * 
+     * @return カードのランク
+     */
     String getRank() {
         return rank;
     }
 
+    /**
+     * カードのスートとランクの文字列を一緒に返す．
+     * 
+     * @return カードの文字列
+     */
+    @Override
     public String toString() {
         return getRank() + " of " + getSuit();
     }
