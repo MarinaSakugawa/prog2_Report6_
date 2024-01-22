901a390d62bf5120f9d8e27e4e73cb304183c129
Marina Sakugawa <e235724@ie.u-ryukyu.ac.jp>
2024-01-23 00:10:08 +0900

    step19

--- app/src/main/java/jp/ac/uryukyu/ie/e235724/Hand.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235724/Hand.java
@@ -2,27 +2,55 @@ package jp.ac.uryukyu.ie.e235724;
 
 import java.util.ArrayList;
 
+/**
+ * ブラックジャックのプレイヤーまたはディーラーの手札を表すクラス．
+ */
 public class Hand {
+
+    /**
+     * 手札に含まれるカードのリスト．
+     */
     private ArrayList<Card> cards = new ArrayList<>();
 
+    /**
+     * Hand クラスのコンストラクタ．
+     */
     Hand() {
 
     }
     
+    /**
+     * 手札のカードリストを取得．
+     * 
+     * @return カードのリスト
+     */
     ArrayList<Card> getCards() {
         return cards;
     }
 
+    /**
+     * 手札にカードを追加する．
+     * 
+     * @param card 追加するカード
+     */
     public void addCard(Card card) {
         cards.add(card);
     }
 
+    /**
+     * 手札のカードを表示する．
+     */
     public void display() {
         for(var card : getCards()) {
             System.out.println(card.toString());
         }
     }
 
+    /**
+     * 手札のカードによる合計スコアを計算する．
+     * 
+     * @return 手札カードの合計スコア
+     */
     public int calculateScore() {
         int score = 0;
         int numAces = 0;
