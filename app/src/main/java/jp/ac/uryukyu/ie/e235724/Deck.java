47ec8ed3ec3f6be4b27caddeaa62719fa08234fd
Marina Sakugawa <e235724@ie.u-ryukyu.ac.jp>
2024-01-23 00:02:08 +0900

    step18

--- app/src/main/java/jp/ac/uryukyu/ie/e235724/Deck.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235724/Deck.java
@@ -3,17 +3,36 @@ package jp.ac.uryukyu.ie.e235724;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * トランプのデッキを表すクラス．
+ */
 public class Deck {
+
+    /**
+     * デッキに含まれるカードのリスト．
+     */
     private ArrayList<Card> cards = new ArrayList<>();
 
+    /**
+     * デッキのカードを取得．
+     * 
+     * @return カードのリスト
+     */
     ArrayList<Card> getCards() {
         return cards;
     }
 
+    /**
+     * デッキのコンストラク．
+     */
     Deck() {
 
     }
 
+    /**
+     * デッキの初期化を行う．
+     * 全てのスートとランクのカード（計52枚分）作成し，デッキに追加する．
+     */
     public void initializeDeck() {
         String[] suits = {"Spade", "Club", "Heart", "Diamond"};
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
@@ -25,10 +44,18 @@ public class Deck {
         }
     }
 
+    /**
+     * デッキのカードをシャッフルする．
+     */
     public void shuffle() {
         Collections.shuffle(cards);
     }
 
+    /**
+     * デッキからカードを一枚引く．
+     * @return 引かれたカード
+     * @throws IllegalStateException デッキが空の場合
+     */
     public Card drawCard() {
         if(cards.isEmpty()) {
             throw new IllegalStateException("Deck is Empty");
